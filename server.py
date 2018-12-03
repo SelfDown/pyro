b3fcd11990a2273c8158c608a6efd52d81c15882
SelfDown <18874948657@163.com>
2018-12-03 17:22:12 +0800

    Update server.py

--- server.py
+++ server.py
@@ -4,31 +4,45 @@ import Pyro.core
 import Pyro.naming
 import select
 Pyro.core.initServer()
-daemon = Pyro.core.Daemon(host="192.168.30.129",port = 7766)
+daemon = Pyro.core.Daemon(host="192.168.30.129",port = 8888)
+import sqlite3
 # locator = Pyro.naming.NameServerLocator()
 #ns = locator.getNS()
 #daemon.useNameServer(ns)
-class test:
+class sqlite_remote():
 	"""docstring for test"""
 	def __init__(self):
 		print "__init__"
+		
 
-	def helloWorld(self):
-		print "say hello world"
-		return "hello world"
+	def execute_sql(self,database,sql):
+		print "getSQLite"
+		conn = sqlite3.connect(database)
+		cursor = conn.cursor()
+		cursor.execute(sql)
+		data = cursor.fetchall()
+		cursor.close()
+		
+		return data
 
-class ObjectImpl(Pyro.core.ObjBase,test):
+	def fetchone(self):
+		return self.fetchone()
+		
+
+		
+
+class ObjectImpl(Pyro.core.ObjBase,sqlite_remote):
 	def __init__(self):
 		Pyro.core.ObjBase.__init__(self)
-		test.__init__(self)
+		sqlite_remote.__init__(self)
 
 obj = ObjectImpl()
 
-daemon.connect(obj,"object")
+daemon.connect(obj,"sqlite")
 def run():
 	while True:
 		sockets = daemon.getServerSockets()
-		ins,outs,exs= select.select(sockets,[],[],2)
+		ins,outs,exs= select.select(sockets,[],[],1)
 		for s in sockets:
 			if s in ins:
 				daemon.handleRequests()
