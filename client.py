10481c877b71376cda4c2d36700fc71d66f6a943
SelfDown <18874948657@163.com>
2018-12-03 17:21:29 +0800

    Update client.py

--- client.py
+++ client.py
@@ -3,11 +3,10 @@
 import Pyro.core
 import Pyro.naming
 Pyro.core.initClient(banner = 0)
-obj = Pyro.core.getProxyForURI("PYROLOC://192.168.30.129:7766/object")
+obj = Pyro.core.getProxyForURI("PYROLOC://192.168.30.129:8888/sqlite")
 
 print "start",obj
-# help(obj)
-print obj.create_client()
-# data = obj.helloWorld()
-# print data
-print "end"
+
+data = obj.execute_sql("database.db","select * from collection_list order by id desc")
+
+print "end",data
